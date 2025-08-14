92844d2b92bf33a2946fa67d9de2009be3e68984
lucas menezes <menezeslucas24082002@gmail.com>
2025-08-14 09:51:33 -0300

    ...

--- exercicioviewsasync/views.py
+++ exercicioviewsasync/views.py
@@ -5,13 +5,13 @@ from django.http import HttpResponse
 async def contador_async(request):
     output = ""
 
-    # Contador de 1 a 5 com 1 segundo entre cada número
+    # Contador de 1 a 5 com 1 segundo entre cada número 
     for num in range(1, 6):
         await asyncio.sleep(1)
-        print(num)  # aparece no terminal
+        print(num)
         output += f"{num}\n"
 
-    # Requisição HTTP para um endpoint confiável
+    # Requisição HTTP 
     async with httpx.AsyncClient() as client:
         r = await client.get("https://api.github.com")
         output += "\n--- Resposta da API ---\n"
